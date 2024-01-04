6c1ba215ef47ebfee7d96e4dc77e787cb5e67507
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2024-01-04 17:03:54 +0530

    sm8250-common: Build Oplus Vibrator Service

--- common.mk
+++ common.mk
@@ -168,7 +168,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony \
     usb \
-    vibrator \
     wfd \
     wlan
 
@@ -209,6 +208,13 @@ PRODUCT_PACKAGES += \
 # Verified boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.oplus
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_PACKAGES += \
